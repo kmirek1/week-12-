6de335ea9baf65262ca44e0d5922d306cc9bf3c5
kmirek1 <karissa.mirek@gmail.com>
2025-11-14 20:36:07 +0000

    chnages

--- week 13  csp/3 — Lists and List Functions.py
+++ week 13  csp/3 — Lists and List Functions.py
@@ -3,6 +3,30 @@
 
 # Topics Covered:
 # Creating lists, indexing, slicing, appending, popping, sorting, reversing.
+# collections are used to store multiple items in a single variable
+#lists are ordered collections of items
+#lists are mutable, meaning you can chnage their content
+#lists are created using brackets[]
+my_list1 = [1, 2, 3, 4, 5]
+#INSTEAD OF CREATING A BUNCH of variables we can create a list 
+# this makes our job easier 
+#when we need to manage multiple items
+# performance task answer
+print(my_list1)
+print(type(my_list1))
+print(my_list1[0]) 
+print(my_list1[1:4]) 
+print(my_list1[0:])
+print(my_list1[-1])
+my_list1.append(6)
+print(my_list1)
+my_list1.extend([7,8,9,10,11,12,13,14])
+print(my_list1)
+# add 500 more numbers 
+my_list1.extend(list(range(15,515)))
+print(my_list1)
+my_list1.extend(list(range(515,1115)))
+print(my_list1)
 
 # Examples:
 
@@ -24,13 +48,18 @@ print(numbers)
 # Practice Problems:
 
 # Create a list with 5 of your favorite foods.
-
+karissa_list = ['pasta', 'ice cream', 'tacos', 'shrimp','pizza']
 # Print the second and last item.
-
+print(karissa_list[1])
+print(karissa_list[-1])
 # Add a new item using .append().
-
+karissa_list.append('strawberrys')
+print(karissa_list)
 # Remove the first item using .pop(0).
-
+karissa_list.pop(0)
+print(karissa_list)
 # Reverse your list using .reverse().
-
+karissa_list.reverse()
+print(karissa_list)
 # Create a list of 3 lists (matrix), and access the middle element.
+matrix_list = []
